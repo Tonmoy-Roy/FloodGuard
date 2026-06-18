289b663569bb370ac79369a52ebab5365d4b56c9
Tonmoy-Roy <roytonmoy901@gmail.com>
2026-06-18 11:55:54 +0600

    some error fixed

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,8 @@
+// postcss.config.mjs
 const config = {
   plugins: {
-    "@tailwindcss/postcss": {},
+    '@tailwindcss/postcss': {}, // Use the official v4 PostCSS compiler plugin
+    autoprefixer: {},
   },
 };
-
 export default config;
